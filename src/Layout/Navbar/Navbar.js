19029f06c6f9b2384b6dde9b25bc3ed9cbfc397a
amolkapadi <amolakapadi@gmail.com>
2023-04-26 00:33:58 +0530

    add icon

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="container">
         <Link className="navbar-brand text-bold" to="/">Solor</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <i class="bi bi-border-style"></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
